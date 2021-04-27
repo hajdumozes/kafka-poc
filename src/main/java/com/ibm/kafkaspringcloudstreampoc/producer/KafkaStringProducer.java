b23b8ae1771ff8c0953a0ce5bb2467e147eeb0a5
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-04-27 13:57:59 +0200

    log sending

--- src/main/java/com/ibm/kafkaspringcloudstreampoc/producer/KafkaStringProducer.java
+++ src/main/java/com/ibm/kafkaspringcloudstreampoc/producer/KafkaStringProducer.java
@@ -1,5 +1,6 @@
 package com.ibm.kafkaspringcloudstreampoc.producer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +8,17 @@ import java.util.UUID;
 import java.util.function.Supplier;
 
 @Service
+@Slf4j
 public class KafkaStringProducer {
 
     @Bean
     public Supplier<String> sendMessages() {
-        return () -> String.format("I will send a random id every second. Id: %s", UUID.randomUUID());
+        return this::logAndSend;
+    }
+
+    private String logAndSend() {
+        UUID uuid = UUID.randomUUID();
+        log.info("Sending random id: {}", UUID.randomUUID());
+        return uuid.toString();
     }
 }
