5c6fabdf9a26800ecbb389580738c98663346fc4
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-04-28 10:40:20 +0200

    make sink constant

--- src/main/java/com/ibm/kafkaspringcloudstreampoc/producer/ScheduledProducer.java
+++ src/main/java/com/ibm/kafkaspringcloudstreampoc/producer/ScheduledProducer.java
@@ -1,8 +1,5 @@
 package com.ibm.kafkaspringcloudstreampoc.producer;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,21 +13,19 @@ import java.util.function.Supplier;
 
 @EnableScheduling
 @Component
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class ScheduledProducer {
-    private final Sinks.Many<UUID> sink = Sinks.many().unicast().onBackpressureBuffer();
+    private final static Sinks.Many<UUID> SINK = Sinks.many().unicast().onBackpressureBuffer();
 
     @Scheduled(initialDelay = 2000, fixedDelay = 4000)
     public void addToFlux() {
         UUID uuid = UUID.randomUUID();
         log.info("Added id ({}) to reactive queue", uuid);
-        sink.tryEmitNext(uuid);
+        SINK.tryEmitNext(uuid);
     }
 
     @Bean
     public Supplier<Flux<UUID>> produceMessagesReactively() {
-        return sink::asFlux;
+        return SINK::asFlux;
     }
 }
