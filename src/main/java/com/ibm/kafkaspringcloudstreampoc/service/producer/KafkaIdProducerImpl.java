8e5b709aebeca416084d63cd6bf780e57930cd63
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-04-30 11:35:27 +0200

    removed unnecessary annotation

--- src/main/java/com/ibm/kafkaspringcloudstreampoc/service/producer/KafkaIdProducerImpl.java
+++ src/main/java/com/ibm/kafkaspringcloudstreampoc/service/producer/KafkaIdProducerImpl.java
@@ -1,6 +1,5 @@
 package com.ibm.kafkaspringcloudstreampoc.service.producer;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
@@ -9,7 +8,6 @@ import java.util.UUID;
 import java.util.function.Supplier;
 
 @Component
-@Slf4j
 class KafkaIdProducerImpl implements KafkaIdProducer {
 
     @Override
